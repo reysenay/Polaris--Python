5ca0e36ec17df57a6e0cacc6cf8e96bbb8928957
Reyhan Sena <140300994+reysenay@users.noreply.github.com>
2025-11-30 18:49:09 +0300

    Add files via upload

---
+++ age_calculator.py
@@ -0,0 +1,7 @@
+current_year= 2025
+
+name= input("Enter your name: ")
+age= int(input("Enter your age:"))
+
+year_100= current_year + (100-age)
+print(f"{name}, you will turn 100 years old in the year {year_100}. ")
---
+++ exam_score.py
@@ -0,0 +1,6 @@
+exam_score= float(input("Enter your exam score: "))
+
+if exam_score > 50:
+    print("You passed the exam!")
+else:
+    print("You failed the exam...")
